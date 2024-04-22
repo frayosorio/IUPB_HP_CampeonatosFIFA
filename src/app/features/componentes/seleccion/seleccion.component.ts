33a6f4096b1353225d8dc25c248973554cda6f03
Fray León Osorio Rivera <frayosorio@gmail.com>
2024-04-22 09:17:45 -0500

    Se agrega rejilla de datos para mostrar los registros de SELECCIONES

--- src/app/features/componentes/seleccion/seleccion.component.ts
+++ src/app/features/componentes/seleccion/seleccion.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { Seleccion } from '../../../core/entidades/Seleccion';
 
 @Component({
   selector: 'app-seleccion',
@@ -9,15 +11,31 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     ReferenciasMaterialModule,
-    FormsModule
+    FormsModule,
+    NgxDatatableModule
   ],
   templateUrl: './seleccion.component.html',
   styleUrl: './seleccion.component.css'
 })
 export class SeleccionComponent {
   public textoBusqueda: string = "";
+  public selecciones: Seleccion[] = [];
+  public columnas = [
+    { name: "Nombre", prop: "nombre" },
+    { name: "Entidad", prop: "entidad" }
+  ];
 
   public buscar() {
 
   }
+  agregar() {
+
+  }
+  modificar() {
+
+  }
+  verificarEliminar() {
+
+  }
+
 }
