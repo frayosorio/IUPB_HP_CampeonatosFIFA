8e1d025c7086a5884b14993ebe84e22cfc9c2e68
Fray León Osorio Rivera <frayosorio@gmail.com>
2024-10-30 07:44:49 -0500

    Se agrega componente para mostrar listas de datos

--- src/app/features/componentes/seleccion/seleccion.component.ts
+++ src/app/features/componentes/seleccion/seleccion.component.ts
@@ -1,22 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
 import { FormsModule } from '@angular/forms';
+import { ColumnMode, NgxDatatableModule, SelectionType } from '@swimlane/ngx-datatable';
+import { Seleccion } from '../../../core/entidades/Seleccion';
+import { SeleccionService } from '../../servicios/seleccion.service';
 
 @Component({
   selector: 'app-seleccion',
   standalone: true,
   imports: [
     ReferenciasMaterialModule,
-    FormsModule
+    FormsModule,
+    NgxDatatableModule
   ],
   templateUrl: './seleccion.component.html',
   styleUrl: './seleccion.component.css'
 })
-export class SeleccionComponent {
+export class SeleccionComponent implements OnInit {
 
   public textoBusqueda: string = "";
+  public selecciones: Seleccion[] = [];
+  public columnas = [
+    { name: "Nombre", prop: "nombre" },
+    { name: "Entidad", prop: "entidad" }
+  ];
+  public modoColumna = ColumnMode;
+  public tipoSeleccion = SelectionType;
+
+  constructor(private seleccionServicio: SeleccionService) {
+  }
+  ngOnInit(): void {
+    this.listar();
+  }
 
   public buscar() {
+
+  }
+
+  public escoger(event: any) {
+
+  }
+
+  public listar() {
+    this.seleccionServicio.listar().subscribe({
+      next: respuesta => {
+        this.selecciones = respuesta;
+      },
+      error: error => {
+        window.alert(error);
+      }
+    });
+  }
+
+  public agregar() {
+
+  }
+
+  public modificar() {
+
+  }
+
+  public verificarEliminar() {
 
   }
 
